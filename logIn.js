c4cb5d13ac92e97b91c2bfe3c024adf48ad8f2fa
Riccardo Di Biasi <dibiasi.1694305@studenti.uniroma1.it>
2018-12-03 14:38:56 +0100

    added login

--- logIn.js
+++ logIn.js
@@ -1,1 +1,16 @@
 //funzione per il login
+
+function login() {
+    var u = JSON.parse(localStorage.utenti);
+    var l = u.length;
+    var username = document.getElementById("userlogin").value;
+    var password = document.getElementById("passlogin").value;
+    for(i=0; i<l; i++){
+        if((u[i].username == username) && (u[i].password == password)) {
+            sessionStorage.setItem("username", u[i].username);
+            return true;
+        }
+    }
+    alert("Utente non registrato");
+    return false;
+}
